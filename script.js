339eecdd5746dd1a77ba02b7adfc8cdee4bcf370
Pi Lanningham <pi.lanningham@gmail.com>
2020-06-09 01:20:09 -0400

    Switch to a dismissable modal
    By using a dismissable modal, we can drag the map around and zoom more naturally.  We also remember whether the user has dismissed the modal.  We use local storage for this, so that we don't have to worry about GDPR cookie policies.

--- script.js
+++ script.js
@@ -1,17 +1,40 @@
 window.onload = function() {
   var header = document.getElementById("vote-header");
-  var sticky = header.offsetTop;
+  var intro = document.getElementById("intro");
+  var dismissBtn = document.getElementById("dismiss-btn");
+  var infoBtn = document.getElementById("info-btn");
+  var content = document.getElementById('intro-content');
 
-  window.onscroll = function() {
-    checkSticky();
-  };
 
-  function checkSticky() {
-    if(window.pageYOffset > sticky) {
-      header.classList.add("sticky");
-    } else {
-      header.classList.remove("sticky");
-    }
+  function hideIntro() {
+    intro.classList.remove("shown", "in-anim");
+    intro.classList.add("hidden", "out-anim");
+
+    content.classList.remove("shown", "in-anim");
+    content.classList.add("hidden", "out-anim");
+
+    header.classList.remove("intro");
   }
-  checkSticky();
+  function showIntro() {
+    intro.classList.remove("hidden", "out-anim");
+    intro.classList.add("shown", "in-anim");
+
+    content.classList.remove("hidden", "out-anim");
+    content.classList.add("shown", "in-anim");
+
+    header.classList.add("intro");
+  }
+
+  if(window.localStorage && window.localStorage.getItem('dismissed')) {
+    hideIntro();
+  } else {
+    showIntro();
+  }
+  dismissBtn.onclick = function() {
+    if(window.localStorage) {
+      window.localStorage.setItem('dismissed', true);
+    }
+    hideIntro();
+  }
+  infoBtn.onclick = showIntro;
 }
