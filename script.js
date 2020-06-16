4d5ca0d5d913ff273cfe14d08c5d9708ae0b7973
Pi Lanningham <pi.lanningham@gmail.com>
2020-06-16 18:23:24 -0400

    Fix for more screen resolutions
    I absolutely hate CSS

--- script.js
+++ script.js
@@ -3,24 +3,25 @@ window.onload = function() {
   var intro = document.getElementById("intro");
   var dismissBtn = document.getElementById("dismiss-btn");
   var infoBtn = document.getElementById("info-btn");
-  var content = document.getElementById('intro-content');
-
+  //var content = document.getElementById('intro-content');
 
   function hideIntro() {
     intro.classList.remove("shown", "in-anim");
     intro.classList.add("hidden", "out-anim");
 
-    content.classList.remove("shown", "in-anim");
-    content.classList.add("hidden", "out-anim");
+    //content.classList.remove("shown", "in-anim");
+    //content.classList.add("hidden", "out-anim");
 
+    header.classList.add("out-anim");
     header.classList.remove("intro");
+    setTimeout(function () { header.classList.remove("out-anim"); }, 500);
   }
   function showIntro() {
     intro.classList.remove("hidden", "out-anim");
     intro.classList.add("shown", "in-anim");
 
-    content.classList.remove("hidden", "out-anim");
-    content.classList.add("shown", "in-anim");
+    //content.classList.remove("hidden", "out-anim");
+    //content.classList.add("shown", "in-anim");
 
     header.classList.add("intro");
   }
